cddc385dfdd7e432667d983eb4d6e738f16cd7b6
阮鸿波 <aronruan@kugou.net>
2020-01-07 16:36:40 +0800

    fix: 手机号码正则

--- src/regexp.ts
+++ src/regexp.ts
@@ -1,6 +1,6 @@
 export const isEmail = (str: string) => /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(str)
 export const isPhoneNumber = (str: string) =>
-  /^0?1[3|4|5|6|7|8][0-9]\d{8}$/.test(str)
+  /^0?1[3|4|5|6|7|8|2|9][0-9]\d{8}$/.test(str)
 export const isIDCard = (str: string) =>
   /^[1-9]\d{5}(18|19|20|(3\d))\d{2}((0[1-9])|(1[0-2]))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$/.test(
     str
